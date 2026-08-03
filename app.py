a917b79e2d9eac237c32309493a3a711e6092cb4
gayathri <192472074.simats@saveetha.com>
2026-08-03 16:41:41 +0530

    Added GitHub Actions Workflow

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 app = Flask(__name__)
 
+# Load trained model
 model = joblib.load("model/model.pkl")
 
 @app.route("/")
@@ -12,7 +13,6 @@ def home():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-
     data = request.get_json()
 
     input_data = pd.DataFrame([[
@@ -21,12 +21,18 @@ def predict():
         data["venue"],
         data["toss_winner"],
         data["toss_decision"]
-    ]])
+    ]], columns=[
+        "team1",
+        "team2",
+        "venue",
+        "toss_winner",
+        "toss_decision"
+    ])
 
     prediction = model.predict(input_data)
 
     return jsonify({
-        "Prediction": int(prediction[0])
+        "Predicted Winner": int(prediction[0])
     })
 
 if __name__ == "__main__":
